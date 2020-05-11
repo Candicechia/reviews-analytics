e8958dbfa4f75ce8dfe1d80a6e7d2e79dfec3991
Candice Lee <candice.lee101@gmail.com>
2020-05-11 19:13:54 +0800

    revised version1

--- read_reviews.py
+++ read_reviews.py
@@ -3,11 +3,19 @@ count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f: #每次讀取一行
 		data.append(line)
+		
 		count += 1 #知道讀取狀態，讓程式每讀10000筆印一次
 		if count % 10000 == 0:
 			print(len(data))
 
 
-print(len(data))
+print('檔案讀取完了, 總共有', len(data), '筆資料')
 
-print(data[0]) #印出讀第一筆reveiw
+
+#計算每筆留言平均長度
+total = 0
+for rvlong in data:
+	total = total + len(rvlong)
+
+average_long = total/len(data)
+print('每筆留言平均長度為:', average_long)
